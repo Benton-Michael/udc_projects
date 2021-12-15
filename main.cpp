c18bbcf87b8b5de69a701ce0b20fbafbba11a34b
Benton-Michael <92832638+Benton-Michael@users.noreply.github.com>
2021-12-15 16:50:54 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#ifndef QUEUE_H;
+#define QUEUE_H;
+#include <iostream>
+using namespace std;
+
+
+
+queue();
+
+
+// The queue class's methods are here:
+// Other methods to add? (pop),
+
+void addToQueue (int a);
+void showCurrentQueue();
+bool emptyQueue();
+
+
+
+
+// The destructor is here
+
+~queue();
+
+};
+
+#endif
+///////////////////////////////////
+/// main entry point
+
+
+int main()
+{
+    cout << queue << endl;
+    return 0;
+}
+
+// This is the template class
+
+template <class t>
+
